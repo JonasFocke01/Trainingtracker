6fbbb5884dc8d5c5414d6d09b98b7ddc439b0f9e
jonas <@DISTRIB_ID=Pop.com>
2024-02-24 21:41:33 +0100

    Trainings are now not longer targeted by index, but by name.
    Trainings can now be targeted from the future (past)

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
 use serde::Deserialize;
 use serde::Serialize;
-use std::io::Write;
 use std::cmp::Ordering;
+use std::io::Write;
 
-#[derive(Serialize, Deserialize, Debug)]
-enum TrainingTodo {
-    Back,
-    Chest,
-    Schoulders,
-    Neck,
-    Arms,
-    Abs,
-}
 const DB_FILE_NAME: &str = ".trainingtracker.json";
 
 #[derive(Serialize, Deserialize)]
 struct TrainingDetails {
-    training_todo: TrainingTodo,
+    name: String,
     rest_days_remaining: u8,
     default_rest_days: u8,
     done_count: usize,
@@ -45,65 +36,57 @@ fn main() {
         .replace_time(time::macros::time!(0:00));
 
     println!("Last run on {:?}", db_file.last_run);
-    db_file.trainings.iter().for_each(|training| {
-        println!(
-            "{:9?} - {}",
-            training.training_todo, training.rest_days_remaining
-        )
-    });
+    db_file
+        .trainings
+        .iter()
+        .for_each(|training| println!("{:9?} - {}", training.name, training.rest_days_remaining));
     println!("\nTodays date is: {:?}", now);
     let days_past = now - db_file.last_run;
     db_file.last_run = now;
     println!("The difference is: {:?} days", days_past.whole_days());
-    db_file.trainings.iter().for_each(|training| {
-        println!(
-            "{:9?} - {}",
-            training.training_todo, training.rest_days_remaining
-        )
-    });
+    db_file
+        .trainings
+        .iter()
+        .for_each(|training| println!("{:9?} - {}", training.name, training.rest_days_remaining));
 
     reduce_training_rest_days_remaining_by(days_past.whole_days() as u8, &mut db_file.trainings);
     let _ = std::fs::write(
         DB_FILE_NAME,
         serde_json::to_string_pretty(&db_file).unwrap(),
     );
+    print!("\nWhat training did you do?\n(Default: None; Training name for not-listed; /* TODO: '-1' for 'did this yesterday' */)\nDue are: ");
     db_file
         .trainings
         .iter()
-        .enumerate()
-        .for_each(|(i, training)| print!("{:?} ({}), ", training.training_todo, i + 1));
+        .filter(|training| training.rest_days_remaining == 0)
+        .for_each(|training| print!("{:?}, ", training.name));
 
     let _ = std::io::stdout().flush();
-    let training_done_str = std::io::stdin().lines().next().unwrap().unwrap();
-    let training_done: u8 = if training_done_str.is_empty() {
+    let selected_training_str = std::io::stdin().lines().next().unwrap().unwrap();
+    let selected_training: &mut TrainingDetails = if selected_training_str.is_empty() {
+        print!("No training selected, abort...");
         return;
+    } else if let Some(selected_training) = db_file
+        .trainings
+        .iter_mut()
+        .find(|training| training.name == selected_training_str)
+    {
+        selected_training
     } else {
-        training_done_str.parse().unwrap()
+        print!("Invalid training {:?}, abort...", selected_training_str);
+        return;
     };
 
-    // reduce done training
-    let mut db_file: DBFile =
-        serde_json::from_str(std::str::from_utf8(&std::fs::read("db.json").unwrap()).unwrap())
-            .unwrap();
-    let training_done = db_file
-        .trainings
-        .get_mut((training_done - 1) as usize)
-        .unwrap();
-    training_done.rest_days_remaining = training_done.default_rest_days;
-    training_done.done_count = training_done.done_count.saturating_add(1);
-    let _ = std::fs::write("db.json", serde_json::to_string_pretty(&db_file).unwrap());
+    selected_training.rest_days_remaining = selected_training.default_rest_days;
+    selected_training.done_count = selected_training.done_count.saturating_add(1);
     let _ = std::fs::write(
         DB_FILE_NAME,
         serde_json::to_string_pretty(&db_file).unwrap(),
     );
 }
 
-fn reduce_training_rest_days_remaining_by(reduce_by: u8, trainings: &mut Vec<TrainingDetails>) {
+fn reduce_training_rest_days_remaining_by(reduce_by: u8, trainings: &mut [TrainingDetails]) {
     trainings.iter_mut().for_each(|training| {
         training.rest_days_remaining = training.rest_days_remaining.saturating_sub(reduce_by);
     })
-}
-
-fn filter_trainings(trainings: &mut Vec<TrainingDetails>) {
-    trainings.retain(|training| training.rest_days_remaining == 0);
 }
