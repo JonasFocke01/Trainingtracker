73da889a3faeef3b479528076a778dc8893c71d1
jonas <@DISTRIB_ID=Pop.com>
2024-02-27 21:08:08 +0100

    Implement save/load functionality with package `JsonStore`

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use json_store_rs::JsonStore;
+use json_store_rs::JsonStoreError;
 use serde::Deserialize;
 use serde::Serialize;
 use std::cmp::Ordering;
 use std::io::Write;
+use std::path::PathBuf;
 
 const DB_FILE_NAME: &str = ".trainingtracker.json";
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct TrainingDetails {
     name: String,
     rest_days_remaining: u8,
@@ -13,17 +16,36 @@ struct TrainingDetails {
     done_count: usize,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct DBFile {
     last_run: time::OffsetDateTime,
     trainings: Vec<TrainingDetails>,
 }
 
+impl Default for DBFile {
+    fn default() -> Self {
+        Self {
+            last_run: time::OffsetDateTime::now_local().unwrap(),
+            trainings: vec![],
+        }
+    }
+}
+
 fn main() {
-    let mut db_file: DBFile =
-        serde_json::from_str(std::str::from_utf8(&std::fs::read(DB_FILE_NAME).unwrap()).unwrap())
-            .unwrap();
-    db_file.trainings.sort_by(|a, b| {
+    let mut path = PathBuf::new();
+    path.push(DB_FILE_NAME);
+    let mut db: JsonStore<DBFile> = JsonStore::load(path.clone()).unwrap_or_else(|err| match err {
+        JsonStoreError::FileNotFound => JsonStore::setup(path).unwrap(),
+        JsonStoreError::PathNotValid => panic!("!PathNotValid!"),
+        JsonStoreError::FilecontentNotValid => unreachable!(),
+        JsonStoreError::FilecontentNotValid_CreatedBackupfile => {
+            panic!("Filecontent was not valid.\nBackup file created.\nplease review")
+        }
+        JsonStoreError::FilecontentNotValid_CouldNotCreateBackupfile => {
+            panic!("Filecontent was not valid.\nNO BACKUP FILE CREATED!")
+        }
+    });
+    db.data_mut().trainings.sort_by(|a, b| {
         if a.rest_days_remaining > b.rest_days_remaining {
             Ordering::Greater
         } else {
@@ -35,27 +57,23 @@ fn main() {
         .unwrap()
         .replace_time(time::macros::time!(0:00));
 
-    println!("Last run on {:?}", db_file.last_run);
-    db_file
-        .trainings
-        .iter()
-        .for_each(|training| println!("{:9?} - {}", training.name, training.rest_days_remaining));
+    println!("Last run on {:?}", db.data().last_run);
     println!("\nTodays date is: {:?}", now);
-    let days_past = now - db_file.last_run;
-    db_file.last_run = now;
+    let days_past = now - db.data().last_run;
+    db.data_mut().last_run = now;
     println!("The difference is: {:?} days", days_past.whole_days());
-    db_file
+
+    reduce_training_rest_days_remaining_by(
+        days_past.whole_days() as u8,
+        &mut db.data_mut().trainings,
+    );
+
+    db.data()
         .trainings
         .iter()
         .for_each(|training| println!("{:9?} - {}", training.name, training.rest_days_remaining));
-
-    reduce_training_rest_days_remaining_by(days_past.whole_days() as u8, &mut db_file.trainings);
-    let _ = std::fs::write(
-        DB_FILE_NAME,
-        serde_json::to_string_pretty(&db_file).unwrap(),
-    );
     print!("\nWhat training did you do?\n(Default: None; Training name for not-listed; /* TODO: '-1' for 'did this yesterday' */)\nDue are: ");
-    db_file
+    db.data()
         .trainings
         .iter()
         .filter(|training| training.rest_days_remaining == 0)
@@ -66,7 +84,8 @@ fn main() {
     let selected_training: &mut TrainingDetails = if selected_training_str.is_empty() {
         print!("No training selected, abort...");
         return;
-    } else if let Some(selected_training) = db_file
+    } else if let Some(selected_training) = db
+        .data_mut()
         .trainings
         .iter_mut()
         .find(|training| training.name == selected_training_str)
@@ -79,10 +98,7 @@ fn main() {
 
     selected_training.rest_days_remaining = selected_training.default_rest_days;
     selected_training.done_count = selected_training.done_count.saturating_add(1);
-    let _ = std::fs::write(
-        DB_FILE_NAME,
-        serde_json::to_string_pretty(&db_file).unwrap(),
-    );
+    let _ = db.write();
 }
 
 fn reduce_training_rest_days_remaining_by(reduce_by: u8, trainings: &mut [TrainingDetails]) {
